667198e1344b44d9bd3a91c731de20e344a7e19f
court-tek <dishoughng@gmail.com>
2023-11-26 15:09:41 -0330

    added some stylesheets including the css reset

--- hello_world.php
+++ hello_world.php
@@ -1,8 +1,13 @@
 <html>
     <head>
         <title>PHP using web server</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="reset.css">
     </head>
     <body>
+        <p>
+            Adding two numbers ( 1 and 2)
+        </p>
         <?php echo '<p>Hello World</p>'; ?>
     </body>
 </html>
