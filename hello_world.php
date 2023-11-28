9ac084dfa5c635e27ea1a32913e0a430cc336e82
court-tek <dishoughng@gmail.com>
2023-11-28 14:15:52 -0330

    changed some styles and addded some sections

--- hello_world.php
+++ hello_world.php
@@ -6,7 +6,21 @@
     $name = "Ebelin";
     $string1 = "Hello ";
     $string2 = "world";
-    $oracion = strtoupper("Hey you owe me money {$name}");
+    $oracion = "<p>" . strtoupper("Hey you owe me money {$name}") . "</p>";
+
+    // understanding numbers in php 
+    $title_numbers_section = "Numbers";
+
+    // understanding conditionals in php
+    $age = 17;
+    
+
+    // understanding arrays in php
+    $title_arrays_section = "Arrays";
+    $cars = array("mercedez", "bmw", "honda", "subaru", ["c63 amg", "m3", "nsx", "wrx sti"]);
+    
+    // understanding loops in php
+
 
     // Main title variable
     const MAIN_TITLE = "This is php"
@@ -20,7 +34,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,100;9..40,200;9..40,300;9..40,400;9..40,500;9..40,600;9..40,700;9..40,800;9..40,900;9..40,1000&family=DM+Serif+Display&display=swap" rel="stylesheet">
     </head>
-    <body>
+    <body>  
         <h1><?php echo MAIN_TITLE; ?></h1>
         <h2><?php echo $title_strings_section; ?></h2>
         <p>
@@ -29,5 +43,8 @@
         <p><?php echo $oracion; ?></p>
         <p><?php echo $string_single_quotes; ?></p>
         <p><?php echo $string1.$string2; ?></p>
+        <h2><?php echo $title_numbers_section; ?></h2>
+        <h2><?php echo $title_arrays_section; ?></h2>
+        <p><?php echo $cars; ?></p>
     </body>
 </html>
